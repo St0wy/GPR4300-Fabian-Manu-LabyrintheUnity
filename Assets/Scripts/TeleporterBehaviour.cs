7928b27603553b81c0eff53d9ad3eec04f9fbbbf
Fabian Huber <stowy@protonmail.ch>
2022-01-20 12:20:30 +0100

    Added teleporter

---
+++ Assets/Scripts/TeleporterBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace LabyrintheUnity
+{
+	public class TeleporterBehaviour : MonoBehaviour
+	{
+		[SerializeField] private Transform end;
+
+		private void OnTriggerEnter2D(Collider2D col)
+		{
+			var rb = col.GetComponent<Rigidbody2D>();
+			if (rb == null)
+			{
+				return;
+			}
+
+			rb.Sleep();
+			rb.position = end.position;
+			rb.WakeUp();
+		}
+	}
+}
