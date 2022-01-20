3b9b2f0167c68b2682f7b21389fb83706cafb5ef
Fabian Huber <stowy@protonmail.ch>
2022-01-20 11:57:37 +0100

    added main title

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace LabyrintheUnity
+{
+	public class MainMenu : MonoBehaviour
+	{
+		[UsedImplicitly]
+		public void LoadLabyrinthScene()
+		{
+			SceneManager.LoadScene(1);
+		}
+
+		[UsedImplicitly]
+		public void QuitApplication()
+		{
+			Application.Quit();
+		}
+	}
+}
