427fec86365c3e4d291d4a08a27d1494e9e78045
Fabian Huber <stowy@protonmail.ch>
2022-01-20 14:54:45 +0100

    added win menu

---
+++ Assets/Scripts/WinDetector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+namespace LabyrintheUnity
+{
+	public class WinDetector : MonoBehaviour
+	{
+		private void OnCollisionEnter2D(Collision2D col)
+		{
+			if (col.gameObject.GetComponent<PlayerInput>() != null)
+			{
+				// Load the wining scene if we collide with the player
+				SceneManager.LoadScene(2);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/WinMenu.cs
@@ -0,0 +1,15 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace LabyrintheUnity
+{
+	public class WinMenu : MonoBehaviour
+	{
+		[UsedImplicitly]
+		public void GoToMainMenu()
+		{
+			SceneManager.LoadScene(0);
+		}
+	}
+}
