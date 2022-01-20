66cd0e22fc79c0ecedaf4c24aeb816cc0640ecbf
Fabian Huber <stowy@protonmail.ch>
2022-01-20 15:24:04 +0100

    animated player

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,22 +4,33 @@ namespace LabyrintheUnity.Player
 {
 	[RequireComponent(typeof(PlayerInputBehaviour))]
 	[RequireComponent(typeof(Rigidbody2D))]
+	[RequireComponent(typeof(Animator))]
 	public class PlayerMovement : MonoBehaviour
 	{
+		private static readonly int IsMoving = Animator.StringToHash("IsMoving");
+
 		private PlayerInputBehaviour playerInput;
+		private Animator animator;
 		private Rigidbody2D rb;
+
 		[SerializeField] private float movementSpeed = 1f;
 
 		private void Awake()
 		{
 			playerInput = GetComponent<PlayerInputBehaviour>();
 			rb = GetComponent<Rigidbody2D>();
+			animator = GetComponent<Animator>();
+		}
+
+		private void Update()
+		{
+			animator.SetBool(IsMoving, rb.velocity != Vector2.zero);
 		}
 
 		private void FixedUpdate()
 		{
 			// Apply movement
-			rb.velocity = playerInput.Movement * movementSpeed * Time.deltaTime;
+			rb.velocity = playerInput.Movement * (movementSpeed * Time.deltaTime);
 		}
 	}
 }
